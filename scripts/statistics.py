34ebc7a70a564c164d12c9dc2d3e38f0583a405c
Miriam Link <mlink@students.uni-mainz.de>
2021-09-27 14:36:35 +0200

    Update scripts/statistics.py

--- scripts/statistics.py
+++ scripts/statistics.py
@@ -1,3 +1,86 @@
 ```
-Hi hier entsteht ein Skript
+Setup
+```
+
+
+
+
+
+```
+average read length
+```
+
+
+
+
+
+
+```
+average contig length
+
+```
+
+
+
+```
+total number of contigs
+
+```
+
+
+
+
+```
+ shortest contig
+
+```
+
+
+
+
+
+```
+longest contig
+
+```
+
+
+
+
+```
+N50 of all contigs
+
+```
+
+
+
+
+
+```
+N50 of all contigs longer than 300bp
+
+```
+
+
+
+
+
+
+
+```
+Decision for best assembly
+
+```
+
+
+
+
+
+
+
+
+
+```
+output
+
 ```
