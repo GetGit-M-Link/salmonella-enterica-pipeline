173283fbadb4ed4c439e3267b8ee36f0b48b1952
Miriam Link <mlink@students.uni-mainz.de>
2021-09-27 17:27:02 +0200

    Update scripts/statistics.py

--- scripts/statistics.py
+++ scripts/statistics.py
@@ -12,10 +12,21 @@ average read length
 ```
 
 
-```
+```k
 Read contig.fasta file
 ```
-
+def read_fasta(file):
+    with open(file, 'r') as fasta:
+    #  contigs = fasta.read().split(">NODE")
+    # Splits fasta file at _length_ to have lines starting with the length (except the first item in the list, which is the text before)
+        dirty_lengths = fasta.read().split("_length_")
+    #remove first item in list and split off the rest of the information so only lengths remain
+    dirty_lengths.pop(0)
+    lengths = []
+    for length in dirty_lengths:
+        lengths.append(int(length.split("_")[0]))
+        
+    print(lengths)
 
 
 ```
