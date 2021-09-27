e6e5158db838dea9651d18577936368a416dd3e2
Miriam Link <mlink@students.uni-mainz.de>
2021-09-27 23:39:57 +0200

    Update scripts/statistics.py

--- scripts/statistics.py
+++ scripts/statistics.py
@@ -131,7 +131,7 @@ Decision for best assembly
 all_assemblies = get_assemblies(sys.argv[1])
 with open("/data/assembled/stats.txt", 'w') as stats:
     for assembly in all_assemblies:
-        stats.write(str(assembly) + "\n")
+        stats.write(str(assembly))
 
 
 
