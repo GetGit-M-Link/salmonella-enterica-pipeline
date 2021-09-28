2bf8a523f9f70e0fbf73e4ff79e3f3efbe486b3d
Miriam Link <mlink@students.uni-mainz.de>
2021-09-28 10:48:14 +0200

    Update scripts/statistics.py

--- scripts/statistics.py
+++ scripts/statistics.py
@@ -186,10 +186,12 @@ for barcode in barcodes:
 
 with open("../data/" + "Analysis.md", 'w') as stats:
     for barcode in masterlist_of_assemblies:
+        make_N50_plot(barcode)
+        stats.write(f"""<img src="../plots/" + {barcode} + "_N50.png" width="400"> \n """)
         for assembly in barcode:
             stats.write(str(assembly))
             make_contig_plots(assembly)
-        make_N50_plot(barcode)
+        
 
 
 
