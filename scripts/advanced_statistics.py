a06d767bd020c41a813cf0f25eeddf83542952d9
Miriam Link <mlink@students.uni-mainz.de>
2021-09-30 15:28:18 +0200

    Update scripts/advanced_statistics.py

---
+++ scripts/advanced_statistics.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+df = pd.read_csv('data/best_data.txt')
+
+g = sns.catplot(data=df, kind="bar",
+    x="barcode", y="N50s", 
+    ci="sd", palette="dark", alpha=.6, height=6
+    )
+    g.despine(left=True)
+    g.set_axis_labels("", "N50")
+    g.legend.set_title("k")
+    plt.savefig("plots/advanced/N50")
+    plt.clf()
+
+g = sns.catplot(data=df, kind="bar",
+    x="barcode", y="assemblylength", 
+    ci="sd", palette="dark", alpha=.6, height=6
+    )
+    g.despine(left=True)
+    g.set_axis_labels("", "assemblylength")
+    g.legend.set_title("k")
+    plt.savefig("plots/advanced/assemblylength")
+    plt.clf()
+
+g = sns.catplot(data=df, kind="bar",
+    x="barcode", y="numberofcontigs", 
+    ci="sd", palette="dark", alpha=.6, height=6
+    )
+    g.despine(left=True)
+    g.set_axis_labels("", "numberofcontigs")
+    g.legend.set_title("k")
+    plt.savefig("plots/advanced/numberofcontigs")
+    plt.clf()
+
+
+
